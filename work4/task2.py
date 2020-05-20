b22a58c15904abf2f5bc4881751d853f07e1e3e6
Robotech7 <mironov.pavel2010@gmail.com>
2020-05-20 14:53:53 +1000

    First work

--- work4/task2.py
+++ work4/task2.py
@@ -8,34 +8,34 @@ class Polynome:
         self.array = array
 
     def __add__(self, other):
-        result = [x + y for x, y in zip_longest(self.array, other.array, fillvalue=0)]
+        result = [x + y for x, y in zip_longest(
+            self.array, other.array, fillvalue=0)]
         return Polynome(result)
 
     def __sub__(self, other):
-        result = [x - y for x, y in zip_longest(self.array, other.array, fillvalue=0)]
+        result = [x - y for x, y in zip_longest(
+            self.array, other.array, fillvalue=0)]
         return Polynome(result)
 
     def __mul__(self, other):
-        result = [x - y for x, y in zip_longest(self.array, other.array, fillvalue=0)]
+        result = [0] * (len(self.array) + len(other.array) - 1)
+        for y1, item1 in enumerate(self.array):
+            for y2, item2 in enumerate(other.array):
+                result[y1 + y2] += item1 * item2
         return Polynome(result)
 
     def calc(self, x):
         result = 0
         for number, item in enumerate(self.array):
             result = result + item * x ** number
-        # y = 0
-        # while y < len(self.array):
-        #     result = result + self.array[y] * x ** y
-        #     y += 1
         return result
 
     def __str__(self):
         return f'Polynome{tuple([x for x in self.array])}'
 
 
-a = Polynome([-5, 15, -16, 11])
-b = Polynome([1, -5, 2, 23])
+a = Polynome([-12, -20])
+b = Polynome([-11, 15, -1])
 
-c = a + b
-print(a.calc(5))
-print(a)
+c = a * b
+print(c.calc(-5))
---
+++ work4/task4.py
@@ -0,0 +1,9 @@
+class Figure:
+
+    def area(self):
+        print('Square')
+
+    def perimetr(self):
+        print('perimetr')
+
+class Triangle
