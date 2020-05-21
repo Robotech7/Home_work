8eb8aa708aa4f6c7e9ac88697d97656b2a9c0f5e
Robotech7 <mironov.pavel2010@gmail.com>
2020-05-21 13:56:48 +1000

    First work

--- work5/task2.py
+++ work5/task2.py
@@ -1,2 +1,12 @@
-file = open('filename.txt', 'r').read()
+import math
 
+file = open('filename.txt', 'r')
+dot_array = []
+for line in file:
+    dot_line = line.split()
+    dot_array.append(list(map(int, dot_line)))
+distance = []
+for p in dot_array:
+    k = math.hypot(p[0] - p[2], p[1] - p[3])
+    distance.append(k)
+print(min(distance))
